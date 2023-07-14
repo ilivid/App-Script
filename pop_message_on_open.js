3380b297ce88ee13e6b39db3b7ae57fa00f0bd07
Livid Su <qq1532250087@gmail.com>
2023-07-14 17:34:42 +0800

    Create pop_message_on_open.js

---
+++ pop_message_on_open.js
@@ -0,0 +1,4 @@
+function onOpen() {
+  let ui = SpreadsheetApp.getUi();
+  ui.alert("This version of Weekly P&L is no longer supported, pls go to the new version: https://docs.google.com/spreadsheets/d/1S5Uh5-gtvgPTLd3n_qnybuM8rw2f0sByRutwmJT12d8")
+}
